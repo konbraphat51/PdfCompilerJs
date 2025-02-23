2437f60dd9b220248b2d8d983508e07178740cbe
konbraphat51 <konbraphat@gmail.com>
2025-02-24 02:44:12 +0900

    override annotation

--- src/domain/objects/PdfBool.js
+++ src/domain/objects/PdfBool.js
@@ -4,6 +4,7 @@ class PdfBool extends PdfObject {
 	/**
 	 * Constructor
 	 * @param {Boolean} value Value of the object
+	 * @override
 	 */
 	constructor(value) {
 		super("Boolean", value)
@@ -13,6 +14,7 @@ class PdfBool extends PdfObject {
 	 * Get value of the object
 	 * @returns {Boolean}
 	 * @readonly
+	 * @override
 	 */
 	get value() {
 		return this.value
@@ -21,6 +23,7 @@ class PdfBool extends PdfObject {
 	/**
 	 * Write string representation of the object
 	 * @returns {String}
+	 * @override
 	 */
 	Write() {
 		return this.value ? "true" : "false"
@@ -30,6 +33,7 @@ class PdfBool extends PdfObject {
 	 * Set value of the object
 	 * @param {Boolean} value Value of the object
 	 * @returns {void}
+	 * @override
 	 */
 	Set(value) {
 		this._value = value
